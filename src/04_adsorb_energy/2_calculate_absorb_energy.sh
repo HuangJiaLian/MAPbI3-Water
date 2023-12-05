454431636ee8f1215137685b33a9237d7bec2da4
Jie Huang <jie.huang@aalto.fi>
2023-12-05 10:46:27 +0200

    add absorb energy calculation-related files

---
+++ src/04_adsorb_energy/2_calculate_absorb_energy.sh
@@ -0,0 +1,24 @@
+calculation_type=ab_energy
+
+sizes=(222 333 444) 
+water_numbs=(156 370 891) 
+
+for i in ${!sizes[@]}
+do
+    size=${sizes[$i]}
+    water_num=${water_numbs[$i]}
+    echo "########### ${size} ${water_num} ##############"
+    cluster_water_log=${calculation_type}/Cluster_${size}/MAPbI3_Water.log
+    cluster_log=${calculation_type}/Cluster_${size}/MAPbI3.log
+    water_log=${calculation_type}/Cluster_${size}/Water.log
+    E_total=$(grep "Total energy: " ${cluster_water_log} | awk '{print $3}')
+    E_cluster=$(grep "Total energy: " ${cluster_log} | awk '{print $3}')
+    E_water=$(grep "Total energy: " ${water_log} | awk '{print $3}')
+    echo Cluster${size}  E_total in a.u. ${E_total}
+    echo Cluster${size}  E_cluster in a.u. ${E_cluster}
+    echo Cluster${size}  E_water in a.u. ${E_water}
+    E_absorb=$(echo "$E_total - $E_cluster - $E_water" | bc)
+    echo Cluster${size} E_absorb in a.u. ${E_absorb}
+    E_absorb_per_water=$(echo "scale=4; $E_absorb / $water_num" | bc)
+    echo Cluster${size} E_absorb per water in a.u. ${E_absorb_per_water}
+done
