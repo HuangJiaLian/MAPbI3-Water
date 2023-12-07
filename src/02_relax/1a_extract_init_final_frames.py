6106aa34d89fd69f7f45b5481480d5801f8791d6
Jie Huang <jie.huang@aalto.fi>
2023-12-08 01:22:19 +0200

    add python and bash scripts to see the structure changes

---
+++ src/02_relax/1a_extract_init_final_frames.py
@@ -0,0 +1,84 @@
+from ase.io import read, write
+from ase.visualize import view
+from ase.geometry.geometry import wrap_positions
+from ase.data import colors, atomic_numbers
+import os
+
+# 000 111 are for creating legends for naked cluster and cluster+water respectively. 
+sizes = ['000', '111', '222', '333', '444']
+cells = []
+
+mainColors = {'Pb': '#747b65', 'I':'#6a0045', 'C':'#a7ac9a', 'N':'#2e4ea8', 'O':'#d61f1f'}
+
+def hex2rgb(color, fraction=True):
+    r = int(color[1:3], 16)
+    g = int(color[3:5], 16)
+    b = int(color[5:], 16)
+    return (r/255., g/255., b/255.) if fraction else (r, g, b) 
+
+for e in ['Pb', 'I', 'C', 'N', 'O']:
+    color = hex2rgb(mainColors[e])
+    e_an = atomic_numbers[e]
+    colors.jmol_colors[e_an] = color
+    colors.cpk_colors[e_an] = color
+
+
+# View used to start ag, and find desired viewing angle
+# view(atoms)
+# rot = '5x,-10y,0z'  # found using ag: 'view -> rotate'
+rot = '0x,0y,0z'  # found using ag: 'view -> rotate'
+
+# Common kwargs for eps, png, pov
+generic_projection_settings = {
+    'rotation': rot,  # text string with rotation (default='' )
+    'radii': .85,  # float, or a list with one float per atom
+    'colors': None,  # List: one (r, g, b) tuple per atom
+    'show_unit_cell': 2,   # 0, 1, or 2 to not show, show, and show all of cell
+}
+
+# Extra kwargs only available for povray (All units in angstrom)
+povray_settings = {
+    'display': False,  # Display while rendering
+    'pause': True,  # Pause when done rendering (only if display)
+    'transparent': True,  # Transparent background
+    #'canvas_width': 2500,  # Width of canvas in pixels
+    'canvas_width': 1000,  # Width of canvas in pixels
+    'canvas_height': None,  # Height of canvas in pixels
+    'camera_dist': 50.,  # Distance from camera to front atom
+    'image_plane': None,  # Distance from front atom to image plane
+    #'camera_type': 'perspective',  # perspective, ultra_wide_angle
+    'point_lights': [],             # [[loc1, color1], [loc2, color2],...]
+    'area_light': [(2., 3., 40.),  # location
+                   'White',       # color
+                   .7, .7, 3, 3],  # width, height, Nlamps_x, Nlamps_y
+    'background': 'White',        # color
+    'textures': None,  # Length of atoms list of texture names
+    'celllinewidth': 0.1,  # Radius of the cylinders representing the cell
+}
+
+
+
+
+# Load init states
+for size in sizes:
+    init_structure = read('cluster_{}/Water_cluster{}.xyz'.format(size, size))
+    write('cluster_{}/Init_{}.cif'.format(size, size), init_structure)
+    # Write the .pov (and .ini) file.
+    povray_settings['textures'] = ['intermediate' for a in init_structure]
+    renderer = write('init_{}.pov'.format(size), \
+        init_structure, **generic_projection_settings, povray_settings=povray_settings)
+    renderer.render()
+    
+    final_structure = read('cluster_{}/MAPbI3-pos-1.xyz'.format(size))
+    final_structure.set_cell(init_structure.cell)
+    write('cluster_{}/Relaxed_{}.cif'.format(size, size), final_structure)
+    # view(final_structure)
+    
+    # Write the .pov (and .ini) file.
+    renderer = write('final_{}.pov'.format(size), \
+        final_structure, **generic_projection_settings, povray_settings=povray_settings)
+    renderer.render()
+cmd = 'rm *.pov *.ini'
+os.system(cmd)
+
+
---
+++ src/02_relax/2a_bond_length.py
@@ -0,0 +1,53 @@
+import MDAnalysis as mda
+import numpy as np 
+import matplotlib.pyplot as plt
+from MDAnalysis.analysis import distances
+import seaborn as sns
+
+
+sizes = ['222', '333', '444']
+labels = ['a', 'b', 'c']
+mainColors = {'Pb': '#747b65', 'I':'#6a0045', 'C':'#a7ac9a',
+              'N':'#576ca8', 'O':'#d61f1f', 'DGreen': '#034c3c'}
+colors = ['#F99005', '#0FA842']
+states = ['Start', 'Relaxed']
+calculation_type='nofix'
+
+plt.figure(figsize=(5.5,5))
+
+for j, opt_idx in enumerate([0, -1]):
+    bond_lengths_list = []
+    for i, size in enumerate(sizes):
+        u = mda.Universe('Cluster_{}/MAPbI3-pos-1.xyz'.format(size))
+        # Point to the opt_idx frame
+        u.trajectory[opt_idx]
+        #  Select all the Pb atoms
+        Pbs = u.select_atoms('name Pb') 
+        idexes = Pbs.ix
+        # print(idexes)
+        
+        bond_lengths = []
+        for Pb in Pbs:
+            # Select all the I atoms around the a Pb atom
+            Is = u.select_atoms('name I').select_atoms('around 5 global index {}'.format(Pb.index))   
+            lengths = distances.distance_array(Is, Pb.position)
+            bond_lengths += lengths.flatten(order='C').tolist()
+        plt.subplot(3, 1, i+1) 
+        plt.ylabel('Density ')
+        plt.xlabel('Pb-I distance [$\AA$]')
+        plt.tick_params(direction="in", axis='both', top=True)
+        plt.xlim(2.8, 4.75)
+        plt.ylim(0, 11)
+        color = mainColors['O'] if opt_idx ==  0 else mainColors['DGreen']
+        plt.hist(bond_lengths, density=True, range=(2, 5), color = color, bins=100, alpha=0.7, label='{}'.format(states[j]))
+        # sns.kdeplot(data=bond_lengths, fill=False, bw_adjust=0.6, color=color)
+        plt.legend(frameon=False)
+        plt.annotate('({})'.format(labels[i]), xy=(0.04, 0.8), xycoords="axes fraction")
+        if i == 0 or i == 1: 
+            plt.tick_params(labelbottom=False)
+            plt.xlabel('')
+        
+plt.subplots_adjust(hspace=0.02, wspace=0.01, left=0.12, bottom=0.1, right=0.95, top=0.95)
+plt.savefig('Figure2.pdf')
+plt.show()
+        
